2b10a1eb2e3fa27af78c970ecabe8f3ae253e916
Nick Chen <nickc@Nicks-MacBook-Air.local>
2022-01-19 14:07:38 -0500

    zz

---
+++ count-zeroes.js
@@ -0,0 +1,23 @@
+function countZeroes(arr) {
+  // add whatever parameters you deem necessary - good luck!
+  let firstZero = findFirst(arr)
+  if (firstZero === -1) return 0;
+
+  return arr.length - firstZero
+}
+
+function findFirst(arr, low = 0, high = arr.length - 1) {
+  if (high >= low) {
+    // Check if mid element is first 0
+    let mid = low + Math.floor((high - low) / 2)
+    if ((mid === 0 || arr[mid - 1] === 1) && arr[mid] === 0) {
+      return mid;
+    } else if (arr[mid] === 1) {
+      return findFirst(arr, mid + 1, high)
+    }
+    return findFirst(arr, low, mid - 1)
+  }
+  return -1;
+}
+
+module.exports = countZeroes
